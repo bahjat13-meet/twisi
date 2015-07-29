12a8d8a5e4538885a9ed7984dbb1bd4ebe58a492
Bahjat <ba_hj_a@hotmail.com>
2015-07-29 16:51:54 +0300

    initial

---
+++ twisi_app/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    url(r'^$', include('twisi.urls', namespace="twisi")),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+]
