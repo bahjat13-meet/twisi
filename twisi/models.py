7882a97514db0d3bc682134e33a70eca34f70b7b
brit13-meet <brit13@meet.mit.edu>
2015-08-03 16:47:58 +0300

    First work on User table

--- twisi/models.py
+++ twisi/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Twisser(models.Model):
+    user = models.OneToOneField(User)
+    nationality = models.CharField(max_length=50)
+    twissies = models.IntegerField()
+
