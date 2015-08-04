b4ac47ef971da7c7c2e3007f8787bcaefeab902b
brit13-meet <brit13@meet.mit.edu>
2015-08-04 16:54:38 +0300

    uploadimage

--- twisi_app/twisi/models.py
+++ twisi_app/twisi/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Twisser(models.Model):
     user = models.OneToOneField(User)
     nationality = models.CharField(max_length=50)
     twissies = models.IntegerField(default=0)
 
+class Drawing(models.Model):
+	user = models.ForeignKey(User)
+	filename = models.CharField(max_length=100)
+	score = models.IntegerField(default=0)
+	category = models.CharField(max_length=20)
+	is_public = models.BooleanField(default=False)
+	date = models.DateTimeField()
