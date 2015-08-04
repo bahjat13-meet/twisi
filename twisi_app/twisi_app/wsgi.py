c57ba74977b48bd6ce294cd83293908cc4804518
Joshua Blum <joshblum@mit.edu>
2015-08-04 17:09:22 +0300

    add wsgi for heroku

--- twisi_app/twisi_app/wsgi.py
+++ twisi_app/twisi_app/wsgi.py
@@ -1,16 +1,15 @@
 """
-WSGI config for twisi_app project.
+WSGI config for twisi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twisi_app.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twisi_app.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
