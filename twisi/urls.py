8e96387db63067a66e4441a66f9ffe8d64e328db
brit13-meet <brit13@meet.mit.edu>
2015-08-04 10:35:28 +0300

    checklogin

--- twisi/urls.py
+++ twisi/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^registration$', views.registration, name='registration'),
     url(r'^$', views.base, name='home'),
-]
+     url(r'^registration$', views.registration, name='registration'),
+      url(r'^check_login$', views.check_login, name='check_login'),
+ ]
