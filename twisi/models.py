c0d4dd6e017be7aea8cbe7dc74d4f7ff042f4cae
brit13-meet <brit13@meet.mit.edu>
2015-08-05 11:31:21 +0300

    probably broken get_image

--- twisi/models.py
+++ twisi/models.py
@@ -8,9 +8,21 @@ class Twisser(models.Model):
     twissies = models.IntegerField(default=0)
 
 class Drawing(models.Model):
-	user = models.ForeignKey(User)
+	twisser = models.ForeignKey(Twisser)
 	filename = models.CharField(max_length=100)
 	score = models.IntegerField(default=0)
 	category = models.CharField(max_length=20)
 	is_public = models.BooleanField(default=False)
 	date = models.DateTimeField()
+
+	def serialize(self):
+		d = {
+			'twisser_id': self.twisser.id,
+			'filename' : self.filename,
+			'score' : self.score,
+			'category' : self.category,
+			'is_public' : self.is_public,
+			'date' : self.date 
+			'drawing_id' : self.id
+		} 
+		return d
--- twisi/views.py
+++ twisi/views.py
@@ -58,7 +58,7 @@ def upload_image(request):
 	user = 	User.objects.get(username = username)
 	if request.method == 'POST':
 		try:
-			drawing = Drawing(user = user ,category = category, date=datetime.datetime.now(pytz.UTC))
+			drawing = Drawing(twisser=user.twisser,category = category, date=datetime.datetime.now(pytz.UTC))
 			drawing.save()
 			filename = 'twisi/static/img/gallery/{0}.png'.format(drawing.id)
 			drawing.filename=filename
@@ -71,11 +71,67 @@ def upload_image(request):
 		return HttpResponse(simplejson.dumps(response), content_type='application/json')
 	else:
 		response = { 'success' : False, 'message' : "the immage didnt post"}
-		return HttpResponse(simplejson.dumps(response), content_type='application/json')	
+		return HttpResponse(simplejson.dumps(response), content_type='application/json')
 
-# Put a try, except around saving the drawings to the database, if there's an error in saving the drawings, return the message in the json
-# Return success false if method isn't POST
-# in registration, if user already exists. call check_login
+
+# parameters (request.POST): username, password, nationality, category, is_public
+# 1. getting own pictures (user.id, doesn't matter if submitted)
+# 2. getting pictures of a certain nationality and category
+# 3. all submitted pictures
+def get_own_images(request):
+	response = login_helper(request)
+	if (response['success'] == False):
+		return response
+	try:	
+		username = request.POST['username']
+	except:
+		response = { 'success' : False, 'message' : "Missing fields"}
+		return HttpResponse(simplejson.dumps(response), content_type='application/json')
+	user = User.objects.get(username=username)
+	drawings = user.twisser.drawing_set.all()
+	drawing_dicts = [drawing.serialize() for drawing in drawings]
+	response = {
+		'success' : True, 'drawings' : drawing_dicts
+	}
+	return HttpResponse(simplejson.dumps(response), content_type='application/json')
+
+
+
+
+def get_image(request):
+	response = login_helper(request)
+	if (response['success'] == False):
+		return response
+	try:	
+		category = request.POST['category']
+		nationality = request.POST['nationality']
+	except:
+		response = { 'success' : False, 'message' : "Missing fields"}
+		return HttpResponse(simplejson.dumps(response), content_type='application/json')
+	drawings = Drawing.objects.filter(twisser__nationality=nationality, category=category).all()
+	drawing_filtering = [drawing.serialize() for drawing in drawings]
+	response = {
+		'success' : True, 'drawings' : drawing_filtering
+	}
+	return HttpResponse(simplejson.dumps(response), content_type='application/json')
+
+	
+
+
+def get_public_images(request):
+	response = login_helper(request)
+	if (response['success'] == False):
+		return response
+	drawings = Drawing.objects.filter(is_public=True).all()
+	drawing_filtering = [drawing.serialize() for drawing in drawings]
+	response = {
+		'success' : True, 'drawings' : drawing_filtering
+	}
+	return HttpResponse(simplejson.dumps(response), content_type='application/json')
+
+	
+	
+	
 
 
 def handle_uploaded_file(f, filename):
