06496a5cc2c1e9679f078a753f44ba410987b7fe
brit13-meet <brit13@meet.mit.edu>
2015-08-05 12:06:02 +0300

    fix url

--- twisi_app/twisi/urls.py
+++ twisi_app/twisi/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.base, name='home'),
      url(r'^registration$', views.registration, name='registration'),
       url(r'^check_login$', views.check_login, name='check_login'),
+	url(r'^upload_image$', views.upload_image, name='upload_image'),
  ]
